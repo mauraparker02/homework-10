f3a61377d1774dc2c9280f752adc3f0f3fdccb5b
Maura Parker <mauraparker02@gmail.com>
2020-04-22 20:42:25 -0400

    HTML route

---
+++ htmlRoute.js
@@ -0,0 +1,17 @@
+var path = require("path");
+
+module.exports = function(app) {
+
+  app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "./notes.html"));
+  });
+
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./index.html"));
+  });
+
+  // If no matching route is found default to home
+  app.get("index", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+};
